4a2e1e7c91af53f880e44c16a44814e5dac18ebc
LTHLTH <xoghk0321@gmail.com>
2019-03-26 23:03:35 +0900

    부분수열의 합 - 재귀

--- 1182.cpp
+++ 1182.cpp
@@ -6,18 +6,22 @@ int res;
 int N, S;
 
 void func(int idx, int sum) {
-    if (idx > N)
+    if (idx == N) {
+        if (sum == S)
+            res++;
         return;
-    if (sum == S)
-        res++;
+    }
+    func(idx + 1, sum);
+    func(idx + 1, sum + arr[idx]);
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> N >> S;
-    for (int i=0; i<N; i++)
+    for (int i = 0; i < N; i++)
         cin >> arr[i];
     func(0, 0);
-    cout << res;
+    
+    cout << res + (!S ? -1 : 0);
 }
