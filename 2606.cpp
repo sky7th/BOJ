df39966faf99f489ba223768f86de9e2a5b944b2
LTHLTH <xoghk0321@gmail.com>
2019-04-15 18:57:53 +0900

    바이러스 - DFS

--- 2606.cpp
+++ 2606.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> birus;
+vector<int> birus[101];
 bool visited[101];
 int ans;
 
 void dfs(int n) {
     visited[n] = true;
     ans++;
-    for (int m = 0; m < birus[n].size(); m++) {
-        if (!visited[birus[n][m]]) dfs(birus[n][m]);
+    for (int i = 0; i < birus[n].size(); i++) { 
+        if (!visited[birus[n][i]]) dfs(birus[n][i]);
     }
     return;
 }
@@ -19,7 +19,6 @@ int main() {
     cin.tie(0);
     int C, N, from, to;
     cin >> C >> N;
-    birus.resize(C + 1);
     for (int i = 0; i < N; i++) {
         cin >> from >> to;
         birus[from].push_back(to);
