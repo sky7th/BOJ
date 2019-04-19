a008185d30f7b8e95f23dcfa91729e309a74f1ec
LTHLTH <xoghk0321@gmail.com>
2019-04-19 21:27:41 +0900

    가장 긴 증가하는 부분 수열 - DP

--- 11053.cpp
+++ 11053.cpp
@@ -1,19 +1,51 @@
+// N*N
 #include <bits/stdc++.h>
 using namespace std;
 
+int A[1001];
+int D[1001];
+int N, res;
+
+int main(void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+    for (int i = 0; i < N; i++) {
+        int len = 0;
+        for (int j = 0; j < i; j++) {
+            if (A[j] < A[i])
+                len = max(len, D[j]);
+        }
+        D[i] = len + 1;
+        res = max(res, D[i]);
+    }
+    cout << res;
+}
+
+// logN* N
+#include <bits/stdc++.h>
+using namespace std;
+
+int A[1001];
+int D[1001];
+
 int main() {
-    ios::sync_with_stdio(0);
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
     int N;
     cin >> N;
-    vector<int> last(1, 0);
-    int cur;
-    for (int i = 0; i < N; i++) {
-        cin >> cur;
-        if (last.back() < cur)
-            last.push_back(cur);
+    for (int i=0; i<N; i++) 
+        cin >> A[i];
+    D[0] = A[0];
+    int len = 1; 
+    for(int i = 1; i < N; ++i) {
+        if (A[i] > D[len-1])
+            D[len++] = A[i];
         else
-            *lower_bound(last.begin(), last.end(), cur) = cur;
+            *lower_bound(D, D + len, A[i]) = A[i];
     }
-    cout << last.size() - 1
+    cout << len;
+    return 0;
 }
