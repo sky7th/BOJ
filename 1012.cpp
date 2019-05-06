47e992a6085d42e91f29cfefae3ad7272053d0cb
LTHLTH <xoghk0321@gmail.com>
2019-05-06 18:17:59 +0900

    유기농 배추 - BFS

--- 1012.cpp
+++ 1012.cpp
@@ -30,14 +30,16 @@ int main() {
                     queue<pair<int, int>> Q;
                     Q.push(make_pair(i, j));
                     while (!Q.empty()) {
-                        auto f = Q.front();
+                        auto cur = Q.front();
                         Q.pop();
                         for (int dir = 0; dir < 4; dir++) {
-                            if (f.Y + dy[dir] < 0 || f.Y + dy[dir] >= N || f.X + dx[dir] < 0 || f.X + dx[dir] >= M)
+                            int ny = cur.Y + dy[dir];
+                            int nx = cur.X + dx[dir];
+                            if (ny < 0 || ny >= N || nx < 0 || nx >= M)
                                 continue;
-                            if (field[f.Y + dy[dir]][f.X + dx[dir]] == 1) {
-                                Q.push(make_pair(f.Y + dy[dir], f.X + dx[dir]));
-                                field[f.Y + dy[dir]][f.X + dx[dir]] = 2;
+                            if (field[ny][nx] == 1) {
+                                Q.push(make_pair(ny, nx));
+                                field[ny][nx] = 2;
                             }
                         }
                     }
@@ -47,3 +49,50 @@ int main() {
         cout << bug << '\n';
     }
 }
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int map[51][51];
+int N, M, K;
+
+void BFS(int p, int q) {
+    if (map[p][q] != 1 || p<0 || p>=N || q<0 || q>=M) return;
+    map[p][q] = 2;
+    BFS(p - 1, q);
+    BFS(p, q - 1);
+    BFS(p, q + 1);
+    BFS(p + 1, q);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T, X, Y;
+    int wiggler;
+    cin >> T;
+    while(T--) {
+        cin >> M >> N >> K;
+
+        for (int i = 0; i < K; i++) {
+            cin >> X >> Y;
+            map[Y][X] = 1;
+        }
+
+        wiggler = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 1) {
+                    wiggler++;
+                    BFS(i, j);
+                }
+            }
+        }
+        cout << wiggler << endl;
+    }
+
+    return 0;
+}
+
+*/
